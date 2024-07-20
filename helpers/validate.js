bfec2e00b78740f450bd9d9e442584432950f962
Edwell22 <edwellkakunguwo358@gmail.com>
2024-07-20 14:47:29 +0200

    new

--- helpers/validate.js
+++
@@ -1,8 +0,0 @@
-const Validator = require('validatorjs');
-const validate = (body, rules, customMessages, callback) => {
-    const validation = new Validator(body, rules, customMessages);
-    validation.passes(() => callback(null, true));
-    validation.fails(() => callback(validation.errors, false));
-}
-
-module.exports = validate;
